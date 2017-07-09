54cd39f048e14f312a512e870160a14172ec402f
John Makridis <j.makridis7@gmail.com>
2017-07-09 19:36:16 +0300

    added ContactCtrl

---
+++ app/controllers/contact-ctrl.js
@@ -0,0 +1,14 @@
+/**
+ * Contact controller 
+ **/
+; (function () {
+
+    angular.module('angularstarter')
+        .controller('ContactCtrl', ['$scope', '$rootScope', '$http', '$state', '$timeout', '$window', 'localStorageService', function ($scope, $rootScope, $http, $state, $timeout, $window, localStorageService) {
+
+
+
+        }])
+
+
+})();
